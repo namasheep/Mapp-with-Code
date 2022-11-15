47a36a6b36147b280b8e4733edde5c792fa03869
Namashivayan Sivaram <nsivaram@uwo.ca>
2022-11-15 18:34:20 -0500

    new start

---
+++ src/main/java/com/mycompany/mapp/Mapp.java
@@ -0,0 +1,20 @@
+
+import com.mycompany.mapp.AppWindow;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+/**
+ *
+ * @author DELL
+ */
+public class Mapp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        AppWindow ll = new AppWindow();
+        
+    }
+}
